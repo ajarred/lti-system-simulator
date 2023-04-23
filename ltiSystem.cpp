a2878aa32da5007692b9b07f960a346311d82896
ajarred <adjarredtan@gmail.com>
2023-04-23 13:48:52 +0800

    Adjust spacing

--- ltiSystem.cpp
+++ ltiSystem.cpp
@@ -20,8 +20,7 @@ void ltiSystem::processFile(const std::string& fileName)
     std::ifstream file;
     std::string line;
     double integerCheck;
-    int nonRecursiveCoefficient;
-    int recursiveCoefficient;
+    int nonRecursiveCoefficient, recursiveCoefficient;
 
     file.open(fileName, std::ios::in);
     if (!file)
@@ -41,16 +40,13 @@ void ltiSystem::processFile(const std::string& fileName)
         "Non-recursive coefficient must be an integer" << std::endl;
         return;
     }
-    
     if(!(std::floor(integerCheck) == integerCheck))
     {
         std::cerr << "Error: Invalid system file\n"
         "Non-recursive coefficient must be an integer" << std::endl;
         return;
     }
-
     // std::cout << "integercheck:" << integerCheck << '\n';
-
     if (integerCheck <= 1)
     {
         std::cerr << "Error: Invalid system file\n" 
@@ -58,32 +54,26 @@ void ltiSystem::processFile(const std::string& fileName)
         "greater than 0" << std::endl;
         return;
     }
-
     nonRecursiveCoefficient = integerCheck;
     nonRecursiveCoefficient -= 1;
-
     // std::cout << "non-recursive coeff:" << nonRecursiveCoefficient << '\n';
 
     std::getline(file, line);
     ss.clear();
     ss.str(line);
-
     if (!(ss >> integerCheck))
     {
         std::cerr << "Error: Invalid system file\n" 
         "Recursive coefficient must be an integer" << std::endl;
         return;
     }
-    
     if(!(std::floor(integerCheck) == integerCheck))
     {
         std::cerr << "Error: Invalid system file\n"
         "Recursive coefficient must be an integer" << std::endl;
         return;
     }
-
     // std::cout << "integercheck:" << integerCheck << '\n';
-
     if (integerCheck <= 0)
     {
         std::cerr << "Error: Invalid system file\n" 
@@ -91,15 +81,12 @@ void ltiSystem::processFile(const std::string& fileName)
         "greater than 0" << std::endl;
         return;
     }
-
     recursiveCoefficient = integerCheck;
-
     // std::cout << "recursive coeff:" << recursiveCoefficient << '\n';
 
     sizeb = nonRecursiveCoefficient+1;
     // std::cout << "sizeb = " << sizeb << '\n';
     bCoeff = new double[sizeb];
-
     for (int i=0; i<sizeb; i++)
     {
         if (!std::getline(file, line))
@@ -108,7 +95,6 @@ void ltiSystem::processFile(const std::string& fileName)
             "Not enough coefficients provided" << std::endl;
             return;
         }
-        
         std::stringstream ss(line);
         double value;
         if (!(ss >> value))
@@ -118,14 +104,12 @@ void ltiSystem::processFile(const std::string& fileName)
             return;
         }
         bCoeff[i] = value;
-
         // std::cout << "bcoeff " << i << " " << bCoeff[i] << '\n';
     }
 
     sizea = recursiveCoefficient;
     // std::cout << "sizea = " << sizea << '\n';
     aCoeff = new double[sizea];
-
     for (int i=0; i<sizea; i++)
     {
         if (!std::getline(file, line))
@@ -134,7 +118,6 @@ void ltiSystem::processFile(const std::string& fileName)
             "Not enough coefficients provided" << std::endl;
             return;
         }
-        
         std::stringstream ss(line);
         double value;
         if (!(ss >> value))
@@ -144,7 +127,6 @@ void ltiSystem::processFile(const std::string& fileName)
             return;
         }
         aCoeff[i] = value;
-
         // std::cout << "acoeff " << i+1 << " " << aCoeff[i] << '\n';
     }
     file.close();
@@ -180,24 +162,20 @@ void ltiSystem::compute_outputs(double* input_samples, int nSamples, double* out
     }
 }
 
-
 bool ltiSystem::isValidSystem() 
 {
     if (aCoeff == nullptr || bCoeff == nullptr) 
     {
         return false;
     }
-
     if (sizea <= 0 || sizeb <= 0) 
     {
         return false;
     }
-
     if (aCoeff[0] == 0.0) 
     {
         return false;
     }
-
     return true;
 }
 
