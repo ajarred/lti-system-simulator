c6fd6357598225d06a0a5658e7d78e82011fd636
ajarred <adjarredtan@gmail.com>
2023-04-24 10:16:54 +0800

    Update help descriptions

--- main.cpp
+++ main.cpp
@@ -20,10 +20,10 @@ string to_lowercase(string str)
 void displayHelp() 
 {
     cout << "Available commands:\n"
-    << "help              : lists available commands of the program\n"
-    << "system filename   : load LTI system coefficients from specified file\n"
-    << "signal filename   : load input signal from specified file\n"
-    << "[number]          : input a floating point number\n"
+    << "help              : provid information how to use the program\n"
+    << "system filename   : extract coefficients of an LTI system from filename\n"
+    << "signal filename   : load input signal from filename\n"
+    << "[number]          : input a floating point number as next input of the system\n"
     << "clear             : clear previous inputs and outputs of the system\n"
     << "cls               : clear the console screen\n"
     << "exit              : exit the program\n";
